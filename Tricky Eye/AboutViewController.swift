f2678ef7f2dd65e9db3fe8b934a2f406bd0d1e9c
Mohamed Alaa <m7med645gamet@gmail.com>
2018-12-08 05:13:14 +0200

    Add files via upload

---
+++ Tricky Eye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mohamed Alaa on 12/7/18.
+//  Copyright © 2018 Mohamed Alaa. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
